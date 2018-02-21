4bee40b9f2b7141747a1de017631226511ada7bb
José Luis Santana <yuniorglez@Mac-Yunior.local>
2018-02-21 17:06:17 +0000

    First commit !

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+// Devuelve EL NOMBRE del mayor de los tres
+function quienEsMayorDeLosTres(p1, p2, p3){
+}
+
+// Devuelve en forma de boolean el resultado
+// NOTA: las mayúsculas y minúsculas no se consideran distintas
+function hayAlgunCorreoRepetido(p1, p2, p3){
+}
+
+// Pregunta al usuario por su edad y devuelve en boolean la respuesta
+function elUsuarioEsMayorQueLosTres(p1, p2, p3){
+}
+
+// Devuelve el número de los que tienen correo acabado en gmail.com
+// RESTRICCIÓN: Resuélvelo usando los métodos .indexOf() ó .lastIndexOf()
+// RESTRICCIÓN: Resuélvelo usando el método .search()
+// RESTRICCIÓN: Resuélvelo usando los métodos .substring() ó substr()
+// RESTRICCIÓN: Plantea otra forma de solucionarlo que no use ninguno de los métodos de arriba
+// Deja descomentada solo una de las soluciones
+function cuantosTienenCorreoDeGmailDotCom(p1, p2, p3){
+}
